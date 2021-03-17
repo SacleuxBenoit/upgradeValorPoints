853489afa04856e9017df2665fbe76b1cfef16be
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-17 12:05:38 +0100

    add the total time that we need for the valor point :arrow_right: index.html && script.js

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ let upgradedGear = document.getElementById('upgradedGear');
 
 let totalValor = document.getElementById('totalValor');
 let totalDungeon = document.getElementById('totalDungeon');
+let totalTime = document.getElementById('totalTime');
 
 function showResult(){
+
+    // Calculate the total valor
     let resultRank = upgradedGear.value - lowGear.value;
     let result = gear.value * resultRank;
     totalValor.textContent = "Il faut au total : " + result + " point de vaillance";
 
-    let numberDungeon = Math.ceil(result / 135);
-    totalDungeon.textContent = "Ce qui équivaut donc à : " + numberDungeon + " donjon";
 
+    // calculate the number of dungeon that we need
+
+    let numberDungeon = Math.ceil(result / 135);
+    totalDungeon.textContent = " - "  + numberDungeon + " donjon";
+
+    // Calculate the total time
+
+    let time = numberDungeon * 12 / 60;
+    totalTime.textContent = " - " + time + " heures, avec une moyenne de 12 minutes par donjon";
 }
