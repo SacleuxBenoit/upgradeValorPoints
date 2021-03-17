87e0711c00646c4eb85eba1ca5c39b88e326cfc2
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-17 12:35:01 +0100

    calcul + display the total of blue and epic quest needed for the valors :arrow_right: index.html && script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let upgradedGear = document.getElementById('upgradedGear');
 let displayValor = document.getElementById('displayValor');
 let displayDungeon = document.getElementById('displayDungeon');
 let displayTime = document.getElementById('displayTime');
+let displayEpicQuest = document.getElementById('displayEpicQuest');
+let displayBlueQuest = document.getElementById('displayBlueQuest');
+
 
 function showResult(){
 
@@ -22,5 +25,15 @@ function showResult(){
     // Calculate the total time
 
     let time = numberDungeon * 12 / 60;
-    displayTime.textContent = " - " + time + " heures, avec une moyenne de 12 minutes par donjon";
+    displayTime.textContent = " - " + time + " heures, avec une moyenne de 12 minutes par donjon +2";
+
+    // Calculate the total of epic quest
+
+    let totalEpicQuest = result / 50;
+    displayEpicQuest.textContent = " - " + totalEpicQuest + " Appels épique";
+
+    // Calculate the total of blue quest
+
+    let totalBlueQuest = result / 35;
+    displayBlueQuest.textContent = " - " + totalBlueQuest + " Appels bleu";
 }
