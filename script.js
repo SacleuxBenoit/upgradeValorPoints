c675a6e83d8acab204cc28af56da1a05998e799b
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-17 12:07:54 +0100

    rename the var for the display :arrow_right: index.html && script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let gear = document.getElementById('gear');
 let lowGear = document.getElementById('lowGear');
 let upgradedGear = document.getElementById('upgradedGear');
 
-let totalValor = document.getElementById('totalValor');
-let totalDungeon = document.getElementById('totalDungeon');
-let totalTime = document.getElementById('totalTime');
+let displayValor = document.getElementById('displayValor');
+let displayDungeon = document.getElementById('displayDungeon');
+let displayTime = document.getElementById('displayTime');
 
 function showResult(){
 
     // Calculate the total valor
     let resultRank = upgradedGear.value - lowGear.value;
     let result = gear.value * resultRank;
-    totalValor.textContent = "Il faut au total : " + result + " point de vaillance";
+    displayValor.textContent = "Il faut au total : " + result + " point de vaillance";
 
 
     // calculate the number of dungeon that we need
 
     let numberDungeon = Math.ceil(result / 135);
-    totalDungeon.textContent = " - "  + numberDungeon + " donjon";
+    displayDungeon.textContent = " - "  + numberDungeon + " donjon";
 
     // Calculate the total time
 
     let time = numberDungeon * 12 / 60;
-    totalTime.textContent = " - " + time + " heures, avec une moyenne de 12 minutes par donjon";
+    displayTime.textContent = " - " + time + " heures, avec une moyenne de 12 minutes par donjon";
 }
