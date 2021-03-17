34a05cf7ad1811e18ab9aeae77739bafacff7d1d
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-17 01:27:25 +0100

    calcul the number of dungeon needed for an upgrade :arrow_right: script.js

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ let lowGear = document.getElementById('lowGear');
 let upgradedGear = document.getElementById('upgradedGear');
 
 let totalValor = document.getElementById('totalValor');
+let totalDungeon = document.getElementById('totalDungeon');
 
 function showResult(){
     let resultRank = upgradedGear.value - lowGear.value;
     let result = gear.value * resultRank;
     totalValor.textContent = "Il faut au total : " + result + " point de vaillance";
+
+    let numberDungeon = Math.ceil(result / 135);
+    totalDungeon.textContent = "Ce qui équivaut donc à : " + numberDungeon + " donjon";
+
 }
