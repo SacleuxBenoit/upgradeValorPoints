eeeb934e7c711622c751b92fb342b8b933880ac7
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-17 01:17:50 +0100

    center the h1 :arrow_right: style.css, create 2 paragraph for totalValor && totalDungeon :arrow_right: index.html, display the result in totalValor with textcontent :arrow_right: script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let gear = document.getElementById('gear');
 let lowGear = document.getElementById('lowGear');
 let upgradedGear = document.getElementById('upgradedGear');
 
+let totalValor = document.getElementById('totalValor');
+
 function showResult(){
     let resultRank = upgradedGear.value - lowGear.value;
     let result = gear.value * resultRank;
-    alert(result)
+    totalValor.textContent = "Il faut au total : " + result + " point de vaillance";
 }
